c6c97d192517b1f9e79a890114f17ec2c2ef196e
is3vd <svdvhmed@gmail.com>
2026-03-14 04:38:25 +0200

    Add files via upload

---
+++ ppants.js
@@ -0,0 +1,7 @@
+// Environment Configuration
+const config = {
+  supabaseUrl: "https://bugtzbxhcnipdvewbiid.supabase.co",
+  supabaseKey:
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Z3R6YnhoY25pcGR2ZXdiaWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjA1NDYsImV4cCI6MjA4ODczNjU0Nn0.v0TeCJEqnaeKswzdVMQ6uQF3SZiVlmRZu5sMWE5SFoU",
+  whatsappNumber: "201033962155",
+};
