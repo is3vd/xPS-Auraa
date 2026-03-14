be825a62f20937252cd95451d19c6c8864b2105d
is3vd <svdvhmed@gmail.com>
2026-03-14 04:59:31 +0200

    Add files via upload

--- hhoofies.js
+++ hhoofies.js
@@ -1,7 +1,7 @@
 // Environment Configuration
 const config = {
-  supabaseUrl: "https://bugtzbxhcnipdvewbiid.supabase.co",
+  supabaseUrl: "https://bxpywdoehphwvpzuifyb.supabase.co",
   supabaseKey:
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ1Z3R6YnhoY25pcGR2ZXdiaWlkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNjA1NDYsImV4cCI6MjA4ODczNjU0Nn0.v0TeCJEqnaeKswzdVMQ6uQF3SZiVlmRZu5sMWE5SFoU",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJ4cHl3ZG9laHBod3ZwenVpZnliIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMxNzk5NTQsImV4cCI6MjA4ODc1NTk1NH0.5kifLbfvP0EfF_clJFCpe6hvVHUUsIr_p691GClgkLg",
   whatsappNumber: "201033962155",
 };
